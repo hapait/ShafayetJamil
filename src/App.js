818e1ac8deddc5dee4e487ee151fcbec35ae48eb
Shafayet Jamil <s.jamil1993@gmail.com>
2019-08-03 01:14:23 +0600

    re upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hay hay hay</h1>
+      <Header/>
     </div>
   );
 }
